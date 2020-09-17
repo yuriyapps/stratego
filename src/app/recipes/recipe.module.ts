69b2530af72ff34e973cd5cf4a41b7ff7541c9ee
yuriyapps <jburshtyko@gmail.com>
2020-09-17 18:26:36 -0400

    Revert "Main files added"
    This reverts commit a3d2b9c6605279a239a85a2c7d1b22c381a3f3df.

--- src/app/recipes/recipe.module.ts
+++
@@ -1,5 +0,0 @@
-import { Ingredient } from '../shared/ingredient.module';
-
-export class Recipe{
-    constructor(public name:string, public description: string, public imagePath: string, public ingredients: Ingredient[]){}
-}
